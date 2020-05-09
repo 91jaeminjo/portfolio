3f92ddb37a4cc2d56eab31f654eebed22a140a83
Jaemin <91jaeminjo@gmail.com>
2020-05-08 23:55:00 -0500

    Added codepen icon at footer, and added feature to change color on hover.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { fab, faGithub, faLinkedin, faCodepen } from '@fortawesome/free-brands-svg-icons'
 
 export default class Footer extends Component {
     render() {
@@ -12,8 +12,9 @@ export default class Footer extends Component {
                     
                         <p className="footer-text">connect via</p>
                     
-                        <li className="list-inline-item"><a className="btn btn-social rounded-circle linkedin" role="button" href="https://www.linkedin.com/in/jae-min-jo" target="_blank"><FontAwesomeIcon icon={faLinkedin} color="gray" /></a></li>
-                        <li className="list-inline-item"><a className="btn btn-social rounded-circle github" role="button" href="https://github.com/91jaeminjo" target="_blank"><FontAwesomeIcon icon={faGithub} color="gray" /></a></li>
+                        <li className="list-inline-item"><a className="btn btn-social rounded-circle" role="button" href="https://www.linkedin.com/in/jae-min-jo" target="_blank"><FontAwesomeIcon icon={faLinkedin} color="gray" /></a></li>
+                        <li className="list-inline-item"><a className="btn btn-social rounded-circle" role="button" href="https://github.com/91jaeminjo" target="_blank"><FontAwesomeIcon icon={faGithub} color="gray" /></a></li>
+                        <li className="list-inline-item"><a className="btn btn-social rounded-circle" role="button" href="#" target="_blank" title="CodePen link coming soon!"><FontAwesomeIcon icon={faCodepen} color="gray" /></a></li>
                     
                 </div>
             </footer>
